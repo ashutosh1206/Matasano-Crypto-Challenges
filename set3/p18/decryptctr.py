7a988530fb6e5b52f78c4d770556eb278cff7279
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-07-13 00:50:58 +0530

    Rectified Error in class implementation

--- set3/p18/decryptctr.py
+++ set3/p18/decryptctr.py
@@ -2,20 +2,27 @@ from Crypto.Cipher import AES
 import struct
 
 key = "YELLOW SUBMARINE"
-nonce = 0
-cnt = 0
+
 
 class secret:
-    def count(nonce, cnt):
-        c1 = struct.pack("<Q", nonce)
-        c2 = struct.pack("<Q", cnt)
-        cnt += 1
+    def __init__(self, nonce, cnt):
+        self.nonce = nonce
+        self.cnt = cnt
+
+    def count(self):
+        c1 = struct.pack("<Q", self.nonce)
+        c2 = struct.pack("<Q", self.cnt)
+        self.cnt += 1
         c3 = c1 + c2
         print c3
         return c3
 
 ciphertext = "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
-
-cipher = AES.new(key, AES.MODE_CTR, counter=secret.count(nonce, cnt))
+ciphertext = ciphertext.decode("base64")
+nonce = 0
+cnt = 0
+obj = secret(nonce,cnt)
+cipher = AES.new(key, AES.MODE_CTR, counter=obj.count)
 print cipher.decrypt(ciphertext)
 
+    
